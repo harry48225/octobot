b21bfb8c7893b1df7763a33df562248c0cba98c5
Harry <harrybest@live.co.uk>
2016-06-27 19:17:22 +0100

    Added permissions, added new commands

--- main.py
+++ main.py
@@ -6,54 +6,45 @@ import threading
 
 #My stuff
 import asteroid
-
-_version_ = '0.01'
-_author_ = 'harry48225'
-
-botnick = 'Octo[bot]'
-realname = 'Octo[bot] {0} Author: {1}'.format(_version_, _author_)
-ident =  botnick
-network = 'irc.freenode.net'
-port = 6667
-channels  = []
-
-commandchar = '_8'
+import areyoubeingwatched
 
 with open('commands.json', 'r') as f:
     
     commands = json.load(f)
+    f.close()
     
-with open('usrandpass', 'r') as f:
+with open('config.json', 'r') as f:
     
-    usrname = f.readline()
-    passwd = f.readline()
+    config = json.load(f)
+    f.close()
+    
+with open('perms.json', 'r') as f:
+    
+    perms = json.load(f)
+    f.close()
+    
+_version_ = config['general']['version']
+_author_ = config['general']['author']
 
-with open('channels', 'r') as f:
-    
-    channels = f.read().rstrip().split()
-
-lastsender = ''
+botnick = config['info']['botnick']
+realname = 'Octo[bot] {0} Author: {1}'.format(_version_, _author_)
+ident =  config['info']['ident']
+passwd = config['info']['pass']
+network = config['network']['network']
+port = config['network']['port']
+channels  = config['channels']
+debug = config['general']['debug']
+commandchar = config['general']['commandchar']
 
 irc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 irc.connect((network, port))
+
     
 def connectandidentify():
-
+        
+    send('PASS {0}'.format(passwd))
     send('NICK {0}'.format(botnick))
     send('USER {0} {1} blah :{2}'.format(ident, network, realname))
-    i = 0
-    while True:
-        print i
-        data = recieve() 
-        print data
-        i += 1
-        if '<password>' in data:
-            print '376 found'
-            break
-    
-    
-    send('MSG NickServ identify {0}'.format(passwd))
-    print 'sent passwd'
     
     
 def sendtochan(string, channel):
@@ -76,33 +67,47 @@ if __name__ == '__main__':
         send('JOIN {0}'.format(channel))
     
     while True:
+        
         data = recieve()
-        print data
         data = data.replace(':', '')
         data = data.split()
-        print data
+        if debug: print data
         
         if data[0] == 'PING':
-            irc.send('PONG\r\n'.encode('UTF-8'))
+            send('PONG :{0}'.format(data[1]))
+            if debug: print 'Pong sent to {0}'.format(data[1])
         
         if data[1] == 'INVITE':
             
             send('JOIN {0}'.format(data[3]))
             
             if data[3] not in channels:
-                channels.append(data[3])
+                config['channels'].append(data[3])
+                
+                with open('config.json', 'w') as f:
+                    
+                    json.dump(config, f, indent=4)
+                    
+                
         
         
         if len(data) > 3:
-            
-                    
-            channel = data[2]  
+         
                 
             if commandchar in data[3]:
                 
+                   
+                    
+                channel = data[2]
+            
+            
+                if debug: print 'Channel is: ' + channel 
+                
                 
                 data[3] = data[3].replace(commandchar, '')
-                print data[3]
+                sendercloak = data[0].split('@')[1]
+                
+                if debug: print 'Command is: ' + data[3]
                 
                 if data[3] == 'help':
                     
@@ -149,39 +154,54 @@ if __name__ == '__main__':
                         
                     elif data[4] == 'channels':
                         
-                        sendtochan('Channels that I am connected to: ' + ', '.join(channels), channel)
+                        sendtochan('Channels that I am in: ' + ', '.join(channels), channel)
                         
                     else:
                         
                         sendtochan("You can only use 'channels' or 'commands'", channel)
                 
                 elif data[3] == 'ping': 
+                    
                     sendtochan('OCTOPONG', channel)
-                    print 'Sending to, ' + channel
+                    if debug: print 'Sending to, ' + channel
                     
                 elif data[3] == 'moo':
+                    
                     sendtochan('moo!', channel)
                     
                 elif data[3] == 'leave':
                     
-                    if len(data) < 5:
-                        
-                        sendtochan('You need another argument. {0}'.format(commands['leave']['syntax']), channel)
-                        
-                    elif data[4] in channels:
-                        
-                        send('PART {0}'.format(data[4]))
-                        channels.remove(data[4])
-                        
+                    if sendercloak == perms['master']:
+                        if len(data) < 5:
+                            
+                            sendtochan('You need another argument. {0}'.format(commands['leave']['syntax']), channel)
+                            
+                        elif data[4] in channels:
+                            
+                            send('PART {0}'.format(data[4]))
+                            channels.remove(data[4])
+                            
+                        else:
+                            
+                            sendtochan('I am not in {0})'.format(data[4]), channel)
+                            
                     else:
                         
-                        sendtochan('I am not connected to {0)'.format(data[4]), channel)
+                        sendtochan('You do not have permission to do that!', channel)
                         
                 elif data[3] == 'are-we-safe?':
                     
                     
                     sendtochan(asteroid.checkifsafe(), channel)
                     
+                elif data[3] == 'am-I-being-watched?':
+                    
+                    if len(data) < 6:
+                        
+                        sendtochan('You need another argument. {0}'.format(commands['am-I-being-watched?']['syntax']), channel)
+                    else:
+                        
+                        sendtochan(areyoubeingwatched.check(data[4], data[5]), channel)
                 else:
                     
                     sendtochan('{0} is not a valid command! Do _8list commands for a list of commands.'.format(data[3]), channel)
